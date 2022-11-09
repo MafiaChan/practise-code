79df5dd8f2ca62197aba81652ae6260e332d44e7
chenzhe968 <chenzhedbl@gmail.com>
2022-11-09 20:39:56 +0800

    22/11/9 作业提交

---
+++ 作业/vueday01/src/作业2-选择喜欢的.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h2>请选择你喜欢的专栏</h2>
+		<span v-for="(item,index) in arr" :key="index">
+			<input type="checkbox" v-model="choose" :value="item"> {{ item }}
+		</span>
+		<ul>
+			<li v-for="(item,index) in choose" :key="index">
+				{{ item }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			arr: ["科幻", "喜剧", "动漫", "冒险", "科技", "军事", "娱乐", "奇闻"],
+			choose: []
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
